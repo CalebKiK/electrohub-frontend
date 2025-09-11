5868efdaa9d6cf152afba67d10475107a7daeb4d
CalebKiK <karimicaleb@gmail.com>
2025-09-11 13:35:30 +0300

    Improve responsive design of homepage sections for smaller screens

--- src/components/Support.js
+++ src/components/Support.js
@@ -7,11 +7,9 @@ const Support = () => {
     <div className="support-container">
       <h1>Customer Support</h1>
       <div className="support-links">
-
-
         <Link to="/warranty" className="support-item">
           <div className="support-icon">
-          <img src="/Icons/warranty.png" alt="warranty Icon" className="warranty-icon-img" />
+            <img src="/Icons/warranty.png" alt="warranty Icon" className="warranty-icon-img" />
           </div>
           <h2>Warranty</h2>
           <p>Check local warranty policy protection</p>
@@ -19,23 +17,23 @@ const Support = () => {
 
         <Link to="/order-support" className="support-item">
           <div className="support-icon">
-          <img src="/Icons/support.png" alt="support Icon" className="support-icon-img" />
+            <img src="/Icons/support.png" alt="support Icon" className="support-icon-img" />
           </div>
           <h2>Order Support</h2>
           <p>Check order progress</p>
         </Link>
 
         <Link to="/FAQs" className="support-item">
-
           <div className="support-icon"></div>
-          <img src="/Icons/faq.png" alt="FAQ Icon" className="faq-icon-img" />
+            <img src="/Icons/faq.png" alt="FAQ Icon" className="faq-icon-img" />
           <h2>FAQs</h2>
           <p>Get help from frequently asked questions</p>
         </Link>
 
         <Link to="/Customer-Feedback" className="support-item">
-        <img src="/Icons/feedback.png" alt="Customer Feedback Icon" className="feedback-icon-img" />
-          <div className="support-icon"></div>
+          <div className="support-icon">
+            <img src="/Icons/feedback.png" alt="Customer Feedback Icon" className="feedback-icon-img" />
+          </div>
           <h2>Customer Feedback</h2>
           <p>Help Us Serve You Better!</p>
         </Link>
